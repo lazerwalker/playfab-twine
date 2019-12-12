7d13fadb9f268fdd57d9ae3290742d50c87093ce
Em Lazer-Walker <me@lazerwalker.com>
2019-12-12 14:56:57 -0500

    Updates

--- index.js
+++ index.js
@@ -13104,7 +13104,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50441" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50712" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
