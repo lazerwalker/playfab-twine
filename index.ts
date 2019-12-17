110bc01459a22cb0aadd095b611abca6ec8b1cb6
Em Lazer-Walker <me@lazerwalker.com>
2019-12-17 13:02:35 -0500

    Pass in State object, rather than using window

--- index.ts
+++ index.ts
@@ -7,18 +7,8 @@ interface HarloweState {
 }
 
 interface Window {
-  setupPlayfab: (trackedVars: string[]) => void;
+  setupPlayfab: (trackedVars: string[], State: HarloweState) => void;
 }
-
-const State: HarloweState = (window as any).State;
-
-const trackedValues = (trackedVariables: string[]) => {
-  let map = {};
-  trackedVariables.forEach(function(v) {
-    map[v] = State.variables[v];
-  });
-  return map;
-};
 
 const createGUID = (): string => {
   //http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
@@ -42,7 +32,15 @@ const getGUID = () => {
   }
 };
 
-window.setupPlayfab = (trackedVariables: string[]) => {
+window.setupPlayfab = (trackedVariables: string[], State: HarloweState) => {
+  const trackedValues = (trackedVariables: string[]) => {
+    let map = {};
+    trackedVariables.forEach(function(v) {
+      map[v] = State.variables[v];
+    });
+    return map;
+  };
+
   var guid = getGUID();
   PlayFab.settings.titleId = "2F970";
 
@@ -61,47 +59,47 @@ window.setupPlayfab = (trackedVariables: string[]) => {
       }
     }
   );
-};
 
-const setUpStateHandlers = (trackedVariables: string[]) => {
-  State.on("forward", e => {
-    PlayFabClient.WritePlayerEvent({
-      EventName: "node_loaded",
-      Body: { text: e, state: trackedValues(trackedVariables) },
-      Timestamp: new Date()
-    });
-    console.log("History event!", e);
-  });
-
-  // Because we're using this in context of Twine 2 + Harlowe,
-  // we can assume jQuery will already exist in the execution environment
-  $(document).on("click", "tw-link", e => {
-    console.log("Tracking link click event: '" + e.target.innerText + "'");
-    PlayFabClient.WritePlayerEvent({
-      EventName: "link_clicked",
-      Body: {
-        text: e.target.innerText,
-        state: trackedValues(trackedVariables)
-      },
-      Timestamp: new Date()
+  const setUpStateHandlers = (trackedVariables: string[]) => {
+    State.on("forward", e => {
+      PlayFabClient.WritePlayerEvent({
+        EventName: "node_loaded",
+        Body: { text: e, state: trackedValues(trackedVariables) },
+        Timestamp: new Date()
+      });
+      console.log("History event!", e);
     });
 
-    PlayFabClient.WritePlayerEvent({
-      EventName: "link_clicked_" + e.target.innerText.replace(/\W/gi, "_"),
-      Body: {
-        text: e.target.innerText,
-        state: trackedValues(trackedVariables)
-      },
-      Timestamp: new Date()
-    });
-  });
+    // Because we're using this in context of Twine 2 + Harlowe,
+    // we can assume jQuery will already exist in the execution environment
+    $(document).on("click", "tw-link", e => {
+      console.log("Tracking link click event: '" + e.target.innerText + "'");
+      PlayFabClient.WritePlayerEvent({
+        EventName: "link_clicked",
+        Body: {
+          text: e.target.innerText,
+          state: trackedValues(trackedVariables)
+        },
+        Timestamp: new Date()
+      });
 
-  window.addEventListener("beforeunload", function(e) {
-    console.log("Tracking browser close with node " + State.passage);
-    PlayFabClient.WritePlayerEvent({
-      EventName: "game_closed",
-      Body: { text: State.passage, state: trackedValues(trackedVariables) },
-      Timestamp: new Date()
+      PlayFabClient.WritePlayerEvent({
+        EventName: "link_clicked_" + e.target.innerText.replace(/\W/gi, "_"),
+        Body: {
+          text: e.target.innerText,
+          state: trackedValues(trackedVariables)
+        },
+        Timestamp: new Date()
+      });
     });
-  });
+
+    window.addEventListener("beforeunload", function(e) {
+      console.log("Tracking browser close with node " + State.passage);
+      PlayFabClient.WritePlayerEvent({
+        EventName: "game_closed",
+        Body: { text: State.passage, state: trackedValues(trackedVariables) },
+        Timestamp: new Date()
+      });
+    });
+  };
 };
